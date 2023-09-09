d9d1f332a72f7acc2951ad9f6f61a8a735324811
Alexis Taha <robertogomezalfa@gmail.com>
2023-09-09 16:42:36 -0300

    Scrollbar añadida para pasar de imagen a imagen

--- cubo_de_datos/appGrafica/main.py
+++ cubo_de_datos/appGrafica/main.py
@@ -25,7 +25,7 @@ fig = None
 ax = None
 canvas = None
 linea_grafico = None  # Variable para almacenar la línea del gráfico
-
+barra_desplazamiento = None  # Variable para la barra de desplazamiento
 
 # Función para cargar la imagen FITS actual
 def cargar_imagen_actual():
@@ -36,13 +36,11 @@ def cargar_imagen_actual():
     canvas.draw()
     actualizar_etiqueta_coordenadas()
 
-
 # Función para actualizar la etiqueta de coordenadas
 def actualizar_etiqueta_coordenadas():
     global coordenadas_label
     if coordenadas_label is not None:
         coordenadas_label.config(text=f"Píxel Seleccionado: ({entrada_coord_x.get()}, {entrada_coord_y.get()})")
-
 
 # Función para cargar la imagen anterior
 def cargar_imagen_anterior():
@@ -51,7 +49,6 @@ def cargar_imagen_anterior():
         imagen_actual -= 1
         cargar_imagen_actual()
 
-
 # Función para cargar la siguiente imagen
 def cargar_siguiente_imagen():
     global imagen_actual
@@ -59,8 +56,14 @@ def cargar_siguiente_imagen():
         imagen_actual += 1
         cargar_imagen_actual()
 
+# Función para cargar la imagen actual en función del valor de la barra de desplazamiento
+def cargar_imagen_desde_barra(event):
+    global imagen_actual
+    nueva_posicion = int(barra_desplazamiento.get())
+    if nueva_posicion >= 0 and nueva_posicion < num_frames:
+        imagen_actual = nueva_posicion
+        cargar_imagen_actual()
 
-# Función para crear una ventana emergente para el gráfico
 # Función para crear una ventana emergente para el gráfico
 def crear_ventana_grafico():
     global ventana_grafico, figura_grafico, axes_grafico, canvas_grafico, ventana_grafico_abierta
@@ -73,7 +76,6 @@ def crear_ventana_grafico():
         ventana_grafico.protocol("WM_DELETE_WINDOW", cerrar_ventana_grafico)
         ventana_grafico_abierta = True
 
-
 # Función para cerrar la ventana emergente del gráfico
 def cerrar_ventana_grafico():
     global ventana_grafico, ventana_grafico_abierta
@@ -85,8 +87,6 @@ def cerrar_ventana_grafico():
         figura_grafico.canvas.draw()
         ventana_grafico_abierta = False
 
-# Función para cargar un archivo FITS
-# Función para cargar un archivo FITS
 # Función para cargar un archivo FITS
 def abrir_archivo():
     global archivo_fits, hdul, datos_cubo, num_frames, boton_anterior, boton_siguiente, boton_graficar
@@ -113,10 +113,10 @@ def abrir_archivo():
             # Habilitar el botón "Graficar"
             boton_graficar.config(state=tk.NORMAL)
             actualizar_etiqueta_coordenadas()  # Agregado para actualizar coordenadas al cargar el archivo
+            actualizar_barra_desplazamiento()
     else:
         # Si no se selecciona un archivo FITS válido, deshabilita el botón "Graficar"
         boton_graficar.config(state=tk.DISABLED)
-
 
 # Función para graficar el espectro del píxel seleccionado
 def graficar():
@@ -175,7 +175,6 @@ def on_image_click(event):
 
         actualizar_etiqueta_coordenadas()
 
-
 # Función para manejar el evento de zoom con la rueda del ratón
 def on_scroll(event):
     global ax
@@ -204,11 +203,18 @@ def on_scroll(event):
         except Exception as e:
             print(f"Error en el zoom: {e}")
 
+# Función para actualizar la barra de desplazamiento
+def actualizar_barra_desplazamiento():
+    global barra_desplazamiento, num_frames
+    if barra_desplazamiento is not None:
+        barra_desplazamiento.config(from_=0, to=num_frames - 1)
+        barra_desplazamiento.set(imagen_actual)
+
 def cerrar_ventana_principal():
     ventana.quit()  # Finalizar el bucle principal de Tkinter
     ventana.destroy()  # Destruir la ventana principal
 
-
+# Crear ventana principal
 ventana = tk.Tk()
 ventana.title("Cargar Archivos Fits")
 ventana.geometry("800x700")
@@ -245,20 +251,19 @@ boton_anterior.grid(row=3, column=0, padx=5, pady=5)
 boton_siguiente = tk.Button(ventana, text="Siguiente", command=cargar_siguiente_imagen, state=tk.DISABLED)
 boton_siguiente.grid(row=3, column=4, padx=5, pady=5)
 
+# Crear una barra de desplazamiento horizontal
+barra_desplazamiento = tk.Scale(ventana, orient="horizontal", command=cargar_imagen_desde_barra)
+barra_desplazamiento.grid(row=3, column=1, columnspan=3, padx=5, pady=5, sticky="ew")
+
 # Crear una figura de Matplotlib y canvas
 fig, ax = plt.subplots(figsize=(6, 6))
 canvas = FigureCanvasTkAgg(fig, master=ventana)
 canvas.get_tk_widget().grid(row=4, column=0, columnspan=5, padx=5, pady=10)
-
 
 # Conectar la función on_scroll al evento de desplazamiento de la rueda del ratón
 fig.canvas.mpl_connect('scroll_event', on_scroll)
 
 # Configurar el evento de clic del ratón en la imagen
 fig.canvas.mpl_connect('button_press_event', on_image_click)
-
-# Configurar el evento de clic del ratón en la imagen
-fig.canvas.mpl_connect('button_press_event', on_image_click)
-
 
 ventana.mainloop()
